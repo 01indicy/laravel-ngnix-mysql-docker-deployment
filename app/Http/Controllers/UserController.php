1965c7ac9e739fc6459454ee11240e05906723d6
indiciez <justinmajura@gmail.com>
2024-04-29 11:58:36 +0300

    Add configuration, environmental, and bootstrap files
    Added several configuration and bootstrap files included .editorconfig, .env.example, .gitignore, .styleci.yml, app.php and others. These files are required to build the structure of the application and ensure smooth running of the system. Various storage and public directories were also updated with .gitignore files to exclude unnecessary files.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller {
+    public function index(): JsonResponse {
+        return response()->json(["msg" => "return all users"]);
+    }
+
+    public function create(Request $request): JsonResponse {
+        $this->validate($request, [
+            "username" => "required",
+            "email" => "required",
+            "password" => "required"
+        ]);
+
+        return response()->json(["msg" => "create user success"]);
+    }
+
+    public function show($id): JsonResponse {
+        return response()->json(["msg" => "show user"]);
+    }
+
+    public function update(Request $request, $id): JsonResponse {
+        $this->validate($request, [
+            "username" => "required",
+            "email" => "required",
+            "password" => "required"
+        ]);
+
+        return response()->json(["msg" => "update user success"]);
+    }
+
+    public function delete($id): JsonResponse {
+        return response()->json(["msg" => "delete user"]);
+    }
+}
---
+++ docker/config/php.dockerfile
@@ -0,0 +1,24 @@
+FROM php:8.2-fpm
+
+RUN apt-get update && apt-get install -y \
+    zlib1g-dev \
+    g++ \
+    git \
+    libicu-dev \
+    libpng-dev \
+    libzip-dev \
+    libonig-dev \
+    unzip \
+    curl \
+    libjpeg62-turbo-dev \
+    libfreetype6-dev
+
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+
+RUN docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
+RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd zip
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+EXPOSE 9000
+CMD ["php-fpm"]
